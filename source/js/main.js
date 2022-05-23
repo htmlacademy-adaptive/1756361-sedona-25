9c10a26215c17dbcbfba468eb57b55f49593965c
Alina <ms.sciurus@gmail.com>
2022-05-23 20:32:15 +0300

    защита2

--- source/js/main.js
+++ source/js/main.js
@@ -9,3 +9,18 @@ button.addEventListener('click', () => {
 
   menu.classList.toggle('site-list--menu');
 })
+
+var map = new GMaps({
+  div: '#gmap',
+  lat: 55.7722200,
+  lng: 37.6155600,
+  zoom: 11
+});
+
+map.addMarkers(
+  [{
+      "lat": "55.767293",
+      "lng": "37.544298",
+      "icon": "source/img/pin.svg"
+  }]
+);
